4e966298f768649bc67066e8e756f35c8d64ff04
Tyler Perata <tylerperata@gmail.com>
2022-12-14 22:07:06 -0800

    Changes

--- client/src/components/landing-components/ProjectDisplay.jsx
+++ client/src/components/landing-components/ProjectDisplay.jsx
@@ -18,15 +18,12 @@ function ProjectDisplay() {
 {
   name: "Hyrule Compendium",
 },
-{
-  name: "Hyrule Compendium",
-},
   ]
 
   return (
-    <section className="w-full">
-        <h2>Projects</h2>
-        <section className="flex flex-wrap w-full">
+    <section className="place-content-center">
+        <h2 className="center">Projects</h2>
+        <section className="flex  flex-wrap justify-around align-center">
             {projects.map(x => (<ProjectCard/>))}
         </section>
     </section>
