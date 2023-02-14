fabf8576a5bfec134d1a1345d3c05e00844733ac
Tyler Perata <tylerperata@gmail.com>
2023-02-13 22:22:07 -0800

    Blog page basic styles

--- client/src/features/blogPost/PostList.jsx
+++ client/src/features/blogPost/PostList.jsx
@@ -7,21 +7,28 @@ function PostList() {
   const posts = useSelector(selectAllPosts);
 
   const renderedPosts = posts.map(x => (
-      <article key={x.id} >
+      <article 
+      className="border-2 rounded-sm p-4 mb-3 w-[95%] mx-auto"
+      key={x.id} 
+      >
 
-        <h3>{x.title}</h3>
-        <p>{x.content}</p>
+        <h3
+        className="bold text-xl mb-2"
+        >{x.title}</h3>
+        <p>{x.content.slice(0,100) + (x.content.length >= 100 ? "..." : "")}</p>
 
       </article>
-  ))
+  ));
 
 
   return (
     <div>
-        <h2>Posts</h2>
+        <h2
+        className="text-4xl border-b-black border-b-2 mb-5 p-4"
+        >Posts</h2>
         {renderedPosts}
     </div>
-  )
+  );
 }
 
 export default PostList
--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -7,7 +7,6 @@ function Blog() {
   return (
     <>
       <Navigation />
-      <div>Blog</div>
       <PostList/>
     </>
   )
