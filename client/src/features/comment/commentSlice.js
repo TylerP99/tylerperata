ea53ecb98ed17281151bcdf2a20ab715d3f6a815
Tyler Perata <tylerperata@gmail.com>
2023-03-08 11:00:39 -0800

    Comment slice

---
+++ client/src/features/comment/commentSlice.js
@@ -0,0 +1,11 @@
+import {createSlice} from "@reduxjs/toolkit";
+
+const initialState = [];
+
+export const commentSlice = createSlice({
+    name: "comment",
+    initialState,
+    reducers: {},
+});
+
+export default commentSlice.reducer;
