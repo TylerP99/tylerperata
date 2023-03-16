05dbae0fcf46fec53e919c8e5de23b21c4b9df88
Tyler Perata <tylerperata@gmail.com>
2023-03-15 22:16:59 -0700

    Update blog post state

--- client/src/features/blogPost/blogPostSlice.js
+++ client/src/features/blogPost/blogPostSlice.js
@@ -1,27 +1,9 @@
 import {createSlice} from "@reduxjs/toolkit";
 
-const initialState = [
-    {
-        id: 1,
-        title: "Learning RTK",
-        content: "It's complicated, thats for sure, but it is also very useful."
-    },
-    {
-        id: 2,
-        title: "MVC Architecture",
-        content: "MVC is an organizational pattern for web applications, consisting of a data model, the views, and controller logic."
-    },
-    {
-        id: 3,
-        title: "Dave Gray Makes Good Tutorials",
-        content: "Currently learning Redux Toolkit using some of his work."
-    },
-    {
-        id: 4,
-        title: "Binding of Isaac is an interesting game",
-        content: "In terms of crazy gameplay, it is weak. However, the style, items, and RNG makes it pretty fun."
-    },
-]
+const initialState = {
+    posts: [],
+    status: "idle" //idle, loading, success, failed
+}
 
 export const blogPostSlice = createSlice({
     name: "blogPost",
