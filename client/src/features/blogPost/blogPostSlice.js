75e086fe3204309b9f8b588a362c02d2e4c19a42
Tyler Perata <tylerperata@gmail.com>
2023-03-16 21:29:23 -0700

    Get posts

--- client/src/features/blogPost/blogPostSlice.js
+++ client/src/features/blogPost/blogPostSlice.js
@@ -1,10 +1,26 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createSlice, createAsyncThunk} from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
     posts: [],
     status: "idle", //idle, loading, success, failed
     message: null,
 }
+
+const POSTS_URL = "http://localhost:5000/api/posts"
+
+export const getPosts = createAsyncThunk("posts/getPosts", async () => {
+    try{
+        const res = await axios.get(POSTS_URL);
+        console.log(res);
+
+        return res;
+    }
+    catch(e) {
+        return e.message;
+    }
+})
+
 
 export const blogPostSlice = createSlice({
     name: "blogPost",
