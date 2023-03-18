e21ae2b1e6a20516b9d3d4e712da2b88b736b367
Tyler Perata <tylerperata@gmail.com>
2023-03-17 21:46:13 -0700

    Other crud operations

--- client/src/features/blogPost/blogPostSlice.js
+++ client/src/features/blogPost/blogPostSlice.js
@@ -19,7 +19,46 @@ export const getPosts = createAsyncThunk("posts/getPosts", async () => {
     catch(e) {
         return e.message;
     }
-})
+});
+
+export const addPost = createAsyncThunk("posts/addPost", async (post) => {
+    try{
+        const res = await axios.post(POSTS_URL, post);
+        
+        console.log(res);
+
+        return res;
+    }
+    catch(e) {
+        return e.message;
+    }
+});
+
+export const updatePost = createAsyncThunk("posts/addPost", async (post, id) => {
+    try{
+        const res = await axios.put(POSTS_URL, {post, id});
+        
+        console.log(res);
+
+        return res;
+    }
+    catch(e) {
+        return e.message;
+    }
+});
+
+export const deletePost = createAsyncThunk("posts/addPost", async (id) => {
+    try{
+        const res = await axios.delete(POSTS_URL, id);
+        
+        console.log(res);
+
+        return res;
+    }
+    catch(e) {
+        return e.message;
+    }
+});
 
 
 export const blogPostSlice = createSlice({
