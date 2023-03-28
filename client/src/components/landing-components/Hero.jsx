c83c83762a148a48f65e386997ff80a3e185c72d
Tyler Perata <tylerperata@gmail.com>
2023-03-27 21:08:31 -0700

    Landing changes

--- client/src/components/landing-components/Hero.jsx
+++ client/src/components/landing-components/Hero.jsx
@@ -11,7 +11,7 @@ function Hero() {
 
       <section className='mb-5'>
         <h1
-        className='flex justify-between items-center text-black bg-green border-2 border-white rounded-full text-5xl transition-opacity animate-growWidth w-[80vw] h-[10vw] p-10 mb-1'
+        className='flex justify-between items-center text-white bg-inherit border-8 border-green rounded-full text-5xl transition-opacity animate-growWidth w-[80vw] h-[10vw] p-10 mb-1'
 
         >
           <FaUser/>
@@ -26,14 +26,14 @@ function Hero() {
       <section className='flex flex-col items-end mb-[4%]'>
 
         <h3
-        className='flex flex-row-reverse items-center justify-between text-black bg-red-500 border-2 border-white rounded-full text-3xl p-10 pr-10 animate-slideLeft mb-3 w-[40vw] h-[5vw]'
+        className='flex flex-row-reverse items-center justify-between text-white bg-inherit border-8 border-red-500 rounded-full text-3xl p-10 animate-slideLeft mb-3 w-[40vw] h-[5vw]'
         >
           <BsStack/>
           Full Stack
           <div></div>
         </h3>
         <h2
-          className='flex flex-row-reverse items-center justify-between text-black bg-red-500 border-2 border-white rounded-full text-5xl p-10 pr-40 animate-slideLeft w-[80vw] h-[10vw]'
+          className='flex flex-row-reverse items-center justify-between text-white bg-inherit border-8 border-red-500 rounded-full text-5xl p-10 animate-slideLeft w-[80vw] h-[10vw]'
         >
           <FaLaptopCode/>
           <span>Web Developer</span>
@@ -47,8 +47,8 @@ function Hero() {
         <p className="text-3xl bold border-b-2 border-white mb-5">Let's make something great together</p>
 
         <section className="w-[40%] flex justify-around items-center mb-5">
-          <Link className="block text-center border border-white px-10 py-5 text-2xl hover:underline hover:bg-white/20" to="/freelancing">Freelancing</Link>
-          <Link className="block text-center border border-white px-10 py-5 text-2xl hover:underline hover:bg-white/20" to="/resume">My Resume</Link>
+          <Link className="block text-center border border-white px-10 py-5 text-2xl hover:underline hover:bg-white/20 hover:shadow-sm hover:shadow-white" to="/freelancing">Freelancing</Link>
+          <Link className="block text-center border border-white px-10 py-5 text-2xl hover:underline hover:bg-white/20 hover:shadow-sm hover:shadow-white" to="/resume">My Resume</Link>
         </section>
 
         <section className="flex flex-col items-center">
