ad205f3d30e914e18e31e5b087005b305f64c6e2
Tyler Perata <tylerperata@gmail.com>
2023-04-11 22:09:21 -0700

    Technologies added to card

--- client/src/features/project/Technology.jsx
+++ client/src/features/project/Technology.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import { FaReact, FaHtml5, FaCss3, FaJs, FaNode,  } from "react-icons/fa";
+import {SiTailwindcss, SiExpress} from "react-icons/si";
+import { TbApi } from "react-icons/tb";
 
-function Technology({Icon, text}) {
+function Technology({text}) {
+
+
+  const selectIcon = (text) => {
+    switch(text.toLowerCase()) {
+      case "react":
+        return FaReact;
+      case "html":
+        return FaHtml5;
+      case "css":
+        return FaCss3;
+      case "javascript":
+        return FaJs;
+      case "nodejs":
+        return FaNode;
+      case "tailwindcss":
+        return SiTailwindcss;
+      case "expressjs":
+        return SiExpress;
+      case "apis":
+        return TbApi;
+      default:
+        return null;
+    }
+  }
+
+  const Icon = selectIcon(text);
+
   return (
     <div className="flex gap-2 items-center border px-2 py-1">
-        {Icon && <Icon/>}
+        {Icon !== null ? <Icon/> : undefined}
         <p>{text}</p>
     </div>
   )
--- client/src/pages/Portfolio.jsx
+++ client/src/pages/Portfolio.jsx
@@ -4,12 +4,19 @@ import { selectAllProjects } from "../features/project/projectSlice";
 
 import PageHeader from '../components/PageHeader'
 
+import Technology from "../features/project/Technology";
+
 function ProjectCard({project}) {
   return (
     <article
     className="border mb-5"
     >
       <h3>{project.name}</h3>
+      {project.technologies.length ? 
+      project.technologies.map(x => <Technology text={x}/>)
+      :
+      undefined
+      }
     </article>
   )
 }
