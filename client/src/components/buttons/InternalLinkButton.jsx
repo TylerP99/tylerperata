9df946ff5c38fceb5e8c044de01a93bfce97a535
Tyler Perata <tylerperata@gmail.com>
2023-05-09 21:19:37 -0700

    Link button

--- client/src/components/buttons/InternalLinkButton.jsx
+++ client/src/components/buttons/InternalLinkButton.jsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
 
-function InternalLinkButton() {
+function InternalLinkButton({text="", to="#", className=""}) {
   return (
-    <div>InternalLinkButton</div>
+    <Link
+    to={to}
+    className={`${className} border text-white bg-black`}
+    >{text}</Link>
   )
 }
 
